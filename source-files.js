3444a0b641841b70086a3057dea7c0ee97f2d386
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2020-09-11 17:42:53 +0000

    deploy: 0b830ce63c2f430a4897705ec0a20bfbd85e64b7

--- source-files.js
+++ source-files.js
@@ -4,4 +4,5 @@ sourcesIndex["chacha20poly1305"] = {"name":"","files":["chacha20poly1305.rs"]};
 sourcesIndex["chacha20poly1305_rfc7539"] = {"name":"","files":["chacha20poly1305.rs"]};
 sourcesIndex["main"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["poly1305"] = {"name":"","files":["poly1305.rs"]};
+sourcesIndex["poly_demo"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
